a5cb350e1c171bfbd9c96170d2cb1a57d0da9bdd
birbjam <41635757+birbjam@users.noreply.github.com>
2019-04-18 21:06:55 -0700

    Added modal function, tried some point mapping

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -91,3 +91,40 @@ function drawMap(points) {
         });
     });
 }
+
+
+//Function for activating the modal.
+function modalTrigger() {
+    // Get the modal
+    var modal = document.getElementById('formModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("modalBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //Prevents the form from being submitted.
+    document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+
+    })
+}
+
